1d99aebcecd2ad1eefa1a776ccfd9a9a31e90047
lidongran128 <490387851@qq.com>
2015-11-27 14:00:19 +0800

    Initial commit

---
+++ app/src/main/java/com/phone178/www/android178phone/MainActivity.java
@@ -0,0 +1,13 @@
+package com.phone178.www.android178phone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
