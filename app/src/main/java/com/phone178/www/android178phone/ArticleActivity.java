73fba0c2b77d32e27abb383b25cc695f263e6253
lidongran128 <490387851@qq.com>
2015-11-30 16:16:04 +0800

    成功显示头条新闻标题

---
+++ app/src/main/java/com/phone178/www/android178phone/ArticleActivity.java
@@ -0,0 +1,101 @@
+package com.phone178.www.android178phone;
+
+import android.annotation.TargetApi;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+/**
+ * Created by 49038 on 2015/11/27.
+ */
+
+import com.phone178.www.android178phone.view.TitleBar;
+
+public class ArticleActivity extends AppCompatActivity {
+    private ImageView mCollectView;
+    private boolean mIsSelected;
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article);
+
+        if (hasKitKat() && !hasLollipop()) {
+            //透明状态栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //透明导航栏
+//                getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        } else if (hasLollipop()) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+
+        final TitleBar titleBar = (TitleBar) findViewById(R.id.title_bar);
+
+        titleBar.setImmersive(true);
+
+        titleBar.setBackgroundColor(Color.parseColor("#64b4ff"));
+
+        titleBar.setLeftImageResource(R.drawable.back_green);
+        titleBar.setLeftText("返回");
+        titleBar.setLeftTextColor(Color.WHITE);
+        titleBar.setLeftClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        titleBar.setTitle("178手机社区");
+        titleBar.setTitleColor(Color.WHITE);
+        titleBar.setSubTitleColor(Color.WHITE);
+        titleBar.setDividerColor(Color.GRAY);
+
+        titleBar.setActionTextColor(Color.WHITE);
+        mCollectView = (ImageView) titleBar.addAction(new TitleBar.ImageAction(R.drawable.collect) {
+            @Override
+            public void performAction(View view) {
+                Toast.makeText(ArticleActivity.this, "点击了收藏", Toast.LENGTH_SHORT).show();
+                mCollectView.setImageResource(R.drawable.fabu);
+                titleBar.setTitle(mIsSelected ? "文章详情\n朋友圈" : "帖子详情");
+                mIsSelected = !mIsSelected;
+            }
+        });
+
+        titleBar.addAction(new TitleBar.TextAction("发布") {
+            @Override
+            public void performAction(View view) {
+                Toast.makeText(ArticleActivity.this, "点击了发布", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public static boolean hasKitKat() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+    }
+
+    public static boolean hasLollipop() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
+    }
+}
